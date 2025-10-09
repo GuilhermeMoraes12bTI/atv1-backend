6e3a9fc0bf80d02ef68d631e7ea788ed80dcd2fe
guilhermemoraes <guilhermemoraesrodrigues2116@gmail.com>
2025-10-08 22:33:55 -0300

    commit

---
+++ src/persons.js
@@ -0,0 +1,21 @@
+const persons = [
+    {
+    name: "Guilherme Moraes",
+    age: 16, 
+    cpf: 12345678900
+},
+
+{
+    name: "Pedrão",
+    age: 30, 
+    cpf: 98765432110
+},
+
+{
+    name: "Davi Lindão",
+    age: 3, 
+    cpf: 45612378920
+}
+]
+
+export {persons}
